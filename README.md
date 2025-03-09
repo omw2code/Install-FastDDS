969b232b9d09345cdbeca2649510bde2fc7e2519
omw2code <brennanrivera0@gmail.com>
2025-03-09 16:54:48 -0400

    Fixed Cloning versions issue, added -v flag that allows control of number of jobs to run when building

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Install-FastDDS
-Install any version of FastDDS locally and its dependencies
+Install any version of FastDDS locally and its dependencies on Debian based systems.
 
-The script installs any version of eProsima's Fast-DDS gloabally via its source code and generates a Fast-DDS example program linked to the installed code on debian systems.
+The script installs any version of eProsima's Fast-DDS gloabally via its source code and generates a Fast-DDS example program linked to the installed code.
 
 __Be aware of relative pathing if the user decides to expand the script.__
 
 Fast-DDS Github: [Github](https://github.com/eProsima/Fast-DDS)
 
-FastDDS is an open-source implementation of the Data Distribution Service (DDS) standard, developed by eProsima. DDS is a middleware protocol designed for real-time, high-performance, secure communication in distributed systems
+FastDDS is an open-source implementation of the Data Distribution Service (DDS) standard, developed by eProsima. DDS is a middleware protocol designed for real-time, high-performance, data-centric secure communication in distributed systems. Fast DDS uses RTPS wire protocl that provides publisher-subscriber communication over transports such as TCP/UDP/IP which guarantees compatability among different DDS implementations.
+
+In the relm of distributed architectures, it is easy to start with a contemporary autonomous vehicle design where there are lots of sensors integrated into a single centralized place. Using DDS allows for a data centric approach which allows you to take the software that was centralized and distribute it so it reduces redundancy. Furthermore, DDS allows for the system to only deal with the data and not get it to different places. The system is then able to reason about the state of the machine rather than focus on what's being said vs. what actually is. 
--- scripts/install-DDS.sh
+++ scripts/install-DDS.sh
@@ -16,46 +16,48 @@ usage() {
 Usage: $(basename "$0") [OPTIONS]
 
 Options:
-  -v : Specifies a version of FastDDS to install
   -c : Clones Fast-DDS, Fast-CDR, Foonathan memory, Fast-DDS-Gen
+  -t : Number of jobs to build cloned repositories. Default is 1.
+  -v : Specifies a version of FastDDS to install
   -h : Shows usage
 EOF
     exit 1
 }
 
 validate_version() {
-    if [ -z "$VERSION" ]; then
+    if [ -z "${VERSION}" ]; then
         echo "Error: Version not specified. Use -v to provide a version." >&2
         usage
     fi
-    if [[ ! $VERSION =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    if [[ ! ${VERSION} =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
         echo "Error: Version must be formatted as v#.#.# (e.g., v2.3.4)." >&2
         usage
     fi
-    if [ -d "$HOME/$VERSION" ]; then
-        echo "Error: $HOME/$VERSION directory exisits." >&2
+    if [ -d "${HOME}/${VERSION}" ]; then
+        echo "Error: ${HOME}/${VERSION} directory exisits." >&2
     fi
 }
 
 build_and_install() {
     local name="$1"
     local path="$2"
+    local threads="$3"
 
-    if [ ! -d "$path" ]; then
-        clone "$path" "../"
+    if [ ! -d "${path}" ]; then
+        clone "${path}" "../"
     fi
-    cd "$path"
+    cd "${path}"
 
-    echo "Building and installing $name..."
+    echo "Building and installing ${name}..."
 
     if [ -d "build" ]; then
-        echo "Removing existing build directory in $name"
+        echo "Removing existing build directory in ${name}"
         rm -rf "build"
     fi
 
     mkdir "build" && cd "build"
     cmake .. -DCMAKE_INSTALL_PREFIX=~/"$VERSION"/install -DBUILD_SHARED_LIBS=ON
-    cmake --build . --target install
+    cmake --build . -j${threads} --target install
     echo "$name built and installed successfully."
     cd - >/dev/null
 }
@@ -65,10 +67,9 @@ clone() {
     local dir="$2"
 
     echo "Cloning $repo..."
-    git clone https://github.com/eProsima/"$repo".git "$dir" || {
-        echo "Error: Failed to clone $repo." >&2
-        exit 1
-    }
+    ( cd ${dir} && git clone https://github.com/eProsima/"$repo".git || {
+        echo "Error: Failed to clone ${repo} because it already exists in ${dir}." >&2
+    } ) 
 }
 
 generate_example_idl() {
@@ -114,25 +115,25 @@ run_fastddsgen() {
 
 # Remove the instance of find_package and add Fast-DDS, Fast-CDR install dir
 update_cmakelists() {
-    local install_dir="$HOME/$VERSION"
+    local install_dir="${HOME}/${VERSION}"
     local temp_file=$(mktemp)
-
+    
     echo "Updating CMakeLists.txt for dependencies..."
-    cat << EOF > "$temp_file"
+    cat << EOF > "${temp_file}"
 # FastDDS dependencies
-link_directories($install_dir/lib libfastrtps)
-link_directories($install_dir/lib libfastcdr)
+link_directories(${install_dir}/lib libfastrtps)
+link_directories(${install_dir}/lib libfastcdr)
 
 # FastDDS dependencies
-include_directories($install_dir/include $install_dir/include/fastcdr)
-link_directories($install_dir/include)
+include_directories(${install_dir}/include ${install_dir}/include/fastcdr)
+link_directories(${install_dir}/include)
 EOF
    
-    sed -i "/find_package/ r $temp_file" CMakeLists.txt
-    sed -i '/find_package/d' CMakeLists.txt
+    sed -i "/find_package/ r ${temp_file}" ../${VERSION}_program/CMakeLists.txt
+    sed -i '/find_package/d' ../${VERSION}_program/CMakeLists.txt
 
     rm -f "$temp_file"
-    export LD_LIBRARY_PATH="${install_dir}/lib":$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH="${install_dir}/lib":${LD_LIBRARY_PATH}
     echo "CMakeLists.txt updated successfully."
 }
 
@@ -141,11 +142,16 @@ if [ $# -eq 0 ]; then
     usage
 fi
 
+# Default vars
+THREADS=1
 REPO_LIST=("foonathan_memory_vendor" "Fast-CDR" "Fast-DDS")
-while getopts 'hcv:' opt; do
+while getopts 'hctv:' opt; do
     case $opt in
         c)
             CLONE=1
+            ;;
+        t)
+            THREADS=${OPTARG}
             ;;
         v)
             VERSION=${OPTARG}
@@ -158,17 +164,17 @@ done
 
 validate_version
 
-if [ -z "$CLONE" ]; then
+if [ -z "${CLONE}" ]; then
     echo "Cloning repositories..."
     for REPO in "${REPO_LIST[@]}"; do 
-        clone "$REPO" "../"
+        clone "${REPO}" "../"
     done
 fi
 
 if [ -n "$VERSION" ]; then
-    echo "Installing FastDDS version $VERSION..."
+    echo "Installing FastDDS version ${VERSION}..."
     for REPO in "${REPO_LIST[@]}"; do 
-        build_and_install "$REPO" "../${REPO}"
+        build_and_install "${REPO}" "../${REPO}" "${THREADS}"
     done
     
     generate_example_idl
